6a53d0a1e9ee8795d7daec14e1ac7e96a65915f6
Jacob Wegner <jwegner@eldarion.com>
2020-02-13 11:30:20 -0600

    add tocs_index

--- sv_mini_atlas/tocs/views.py
+++ sv_mini_atlas/tocs/views.py
@@ -1,9 +1,25 @@
 import os
 
 from django.conf import settings
-from django.http import FileResponse, Http404
+from django.core.cache import cache
+from django.http import FileResponse, Http404, JsonResponse
+from django.urls import reverse
+
 
 TOC_DATA_PATH = os.path.join(settings.PROJECT_ROOT, "data", "tocs")
+CACHE_FOREVER = None
+
+
+def tocs_index(request):
+    key = "tocs-index"
+    data = cache.get(key)
+    if data is None:
+        data = []
+        files = [f for f in os.listdir(TOC_DATA_PATH) if f.count(".json")]
+        for filename in files:
+            data.append(reverse("serve_toc", args=[filename]))
+        cache.set(key, data, CACHE_FOREVER)
+    return JsonResponse({"tocs": data})
 
 
 def serve_toc(request, filename):
