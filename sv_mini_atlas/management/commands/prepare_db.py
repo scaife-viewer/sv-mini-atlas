9eaf3c9813e6d2912ba312863e951e1154a2a42d
Jacob Wegner <jwegner@eldarion.com>
2020-09-25 16:27:47 -0500

    ensure stages actually complete

--- sv_mini_atlas/management/commands/prepare_db.py
+++ sv_mini_atlas/management/commands/prepare_db.py
@@ -1,5 +1,5 @@
+import concurrent.futures
 import os
-from concurrent.futures import ProcessPoolExecutor as PreferredExecutor
 
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
@@ -10,7 +10,6 @@ from scaife_viewer.atlas import importers, tokenizers
 from scaife_viewer.atlas.conf import settings
 
 
-# @@@ better way to alias this from our conf module?
 WORKER_COUNT = settings.SCAIFE_VIEWER_ATLAS_INGESTION_CONCURRENCY
 
 
@@ -20,6 +19,30 @@ class Command(BaseCommand):
     """
 
     help = "Prepares the database"
+
+    def do_stage(self, stage):
+        exceptions = False
+        with Timer() as t:
+            fs = {}
+            with concurrent.futures.ProcessPoolExecutor(
+                max_workers=WORKER_COUNT
+            ) as executor:
+                for msg, callback in stage["callbacks"]:
+                    print(f"--[{msg}]--")
+                    fs[executor.submit(callback)] = msg
+            for f in concurrent.futures.as_completed(fs):
+                msg = fs[f]
+                try:
+                    f.result()
+                except Exception as exc:
+                    exceptions = True
+                    print("{} generated an exception: {}".format(msg, exc))
+        print(f"{stage['name']} {t.elapsed}")
+
+        if exceptions:
+            raise AssertionError(
+                f"Exceptions were encountered in running {stage['name']}"
+            )
 
     def handle(self, *args, **options):
         if os.path.exists("db.sqlite3"):
@@ -36,50 +59,46 @@ class Command(BaseCommand):
             importers.versions.import_versions(reset=True)
         print(f"{t.elapsed}")
 
-        async_stages_1 = [
-            ("Loading alignments", importers.alignments.import_alignments),
-            (
-                "Loading text annotations",
-                importers.text_annotations.import_text_annotations,
-            ),
-            (
-                "Loading metrical annotations",
-                importers.metrical_annotations.import_metrical_annotations,
-            ),
-            (
-                "Loading image annotations",
-                importers.image_annotations.import_image_annotations,
-            ),
-            (
-                "Loading audio annotations",
-                importers.audio_annotations.import_audio_annotations,
-            ),
-        ]
-        with Timer() as t:
-            with PreferredExecutor(max_workers=WORKER_COUNT) as executor:
-                for msg, stage in async_stages_1:
-                    print(f"--[{msg}]--")
-                    executor.submit(stage)
-        print(f"async_stages_1 {t.elapsed}")
+        stage_1 = {
+            "name": "stage 1",
+            "callbacks": [
+                ("Loading alignments", importers.alignments.import_alignments),
+                (
+                    "Loading text annotations",
+                    importers.text_annotations.import_text_annotations,
+                ),
+                (
+                    "Loading metrical annotations",
+                    importers.metrical_annotations.import_metrical_annotations,
+                ),
+                (
+                    "Loading image annotations",
+                    importers.image_annotations.import_image_annotations,
+                ),
+                (
+                    "Loading audio annotations",
+                    importers.audio_annotations.import_audio_annotations,
+                ),
+            ],
+        }
+        self.do_stage(stage_1)
 
         with Timer() as t:
             self.stdout.write("--[Tokenizing versions/exemplars]--")
             tokenizers.tokenize_all_text_parts(reset=True)
         print(f"tokenizers ran {t.elapsed}")
 
-        async_stages_2 = [
-            (
-                "Loading token annotations",
-                importers.token_annotations.apply_token_annotations,
-            ),
-            (
-                "Loading named entity annotations",
-                importers.named_entities.apply_named_entities,
-            ),
-        ]
-        with Timer() as t:
-            with PreferredExecutor(max_workers=WORKER_COUNT) as executor:
-                for msg, stage in async_stages_2:
-                    print(f"--[{msg}]--")
-                    executor.submit(stage)
-        print(f"stage2 {t.elapsed}")
+        stage_2 = {
+            "name": "stage 2",
+            "callbacks": [
+                (
+                    "Loading token annotations",
+                    importers.token_annotations.apply_token_annotations,
+                ),
+                (
+                    "Loading named entity annotations",
+                    importers.named_entities.apply_named_entities,
+                ),
+            ],
+        }
+        self.do_stage(stage_2)
