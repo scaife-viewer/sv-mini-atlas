e3844a46c5b4f56fb4d673f5de22ddd29d4923b8
Jacob Wegner <jwegner@eldarion.com>
2020-09-28 12:09:34 -0500

    fall back to concurrency default
    defaults to None, which lets the OS decide how many processes to use

--- sv_mini_atlas/settings.py
+++ sv_mini_atlas/settings.py
@@ -196,10 +196,3 @@ ATLAS_CONFIG = dict(
 )
 
 NODE_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-
-# @@@ we will hit some timeouts with SQLite if we don't override this during ingestion, which I realize now we totally could
-# maybe do here too
-SCAIFE_VIEWER_ATLAS_INGESTION_CONCURRENCY = int(
-    os.environ.get("SCAIFE_VIEWER_ATLAS_INGESTION_CONCURRENCY", 1)
-)
