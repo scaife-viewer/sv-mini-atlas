a05399cdcec34ec6f6e24ebc564067152bd01bc8
Jacob Wegner <jwegner@eldarion.com>
2021-07-08 17:11:08 -0500

    Make a really rough pass at importing records
    Still need to create the standoff annotation

--- data-prep/parrish-alignment/script.py
+++ data-prep/parrish-alignment/script.py
@@ -156,3 +156,135 @@ for pair in pairs:
     for fragment in pair:
         print(*fragment[1], end=" ")
     print()
+
+
+with open(pairs_path) as f:
+    pairs = json.load(f)
+
+from collections import Counter
+
+
+
+def resolve_pairs(pairs, greek_version):
+    """
+    This should be done after running `prepare_db`
+    """
+
+    aleph = 'ʼ'
+    counter = 0
+    new_pairs = []
+    for pair in pairs:
+        new_pair = []
+
+        token_counter = Counter()
+        ref = f'1.{pair[0][0].split(".")[0]}'
+        greek_tokens = Token.objects.filter(text_part__urn=f"{greek_version.urn}{ref}")
+        english_content = []
+        position = 1
+        for record in pair:
+            greek_ve_refs = []
+            for tv in record[2]:
+                tw = Token.get_word_value(tv)
+                non_numeric_tw = re.compile(r"\d").sub("", tw)
+                relevant = list(greek_tokens.filter(
+                    Q(word_value=tw) |
+                    Q(word_value=f"{tw}ʼ") |
+                    Q(word_value=non_numeric_tw)
+                ))
+                if not relevant:
+                    # NOTE: These could be things where the sentence boundaries from treebank
+                    # are useful; skip for now
+                    # if tw and tw not in ["ἀντιάσας", "κέν", "πάντων", "ἐν", "ἑοῖσι", "στήθεσσιν", "οὐ", "δ"]:
+                    #     assert False
+                    # else:
+                    #     continue
+                    greek_ve_refs.append(None)
+                    continue
+
+                token = None
+                if len(relevant) == 1:
+                    token = relevant[0]
+                else:
+                    key = relevant[0].word_value
+                    if key not in token_counter:
+                        token_counter[key] = 0
+                    token = relevant[token_counter[key]]
+                    token_counter[key] += 1
+                greek_ve_refs.append(token.ve_ref)
+
+            eng_ve_refs = []
+            for word in record[1]:
+                for possible_word in word.split():
+                    w = possible_word.strip()
+                    if w:
+                        eng_ve_refs.append(
+                            f"{ref}.t{position}"
+                        )
+                        position += 1
+            new_record = [
+                eng_ve_refs,
+                greek_ve_refs
+            ]
+            new_pair.append(new_record)
+        new_pairs.append(new_pair)
+    return new_pairs
+
+greek_version = Node.objects.get(urn="urn:cts:greekLit:tlg0012.tlg001.perseus-grc2:")
+new_pairs = resolve_pairs(pairs, greek_version)
+
+json.dump(new_pairs, open("data-prep/parrish-alignment/new-pairs.json", "w"), indent=2, ensure_ascii=False)
+
+new_pairs = json.load(open("data-prep/parrish-alignment/new-pairs.json"))
+
+
+def create_alignment_records(new_pairs, greek_version, english_version):
+    alignment_urn = "urn:cite2:scaife-viewer:alignment.v1:iliad-word-alignment-parrish-998078bc3bab42978b47fa8e8b852cae"
+    ta = TextAlignment.objects.create(
+        label="Iliad Word Alignment",
+        urn=alignment_urn,
+        # format="atlas-standoff-annotation",
+    )
+    ta.versions.set([english_version, greek_version])
+    alignment = ta
+
+    version_objs = [english_version, greek_version]
+
+    idx = 0
+    shared_urn_part = alignment_urn.rsplit(":", maxsplit=1)[1]
+    for pair in new_pairs:
+        for entry in pair:
+            urn = f"urn:cite2:scaife-viewer:alignment-record.v1:{shared_urn_part}_{idx}"
+            record = TextAlignmentRecord(
+                idx=idx,
+                alignment=alignment,
+                urn=urn
+            )
+            record.save()
+            idx += 1
+
+            # stop at 2pm
+            for version_obj, relation in zip(version_objs, entry):
+                relation_obj = TextAlignmentRecordRelation(version=version_obj, record=record)
+                relation_obj.save()
+
+                tokens = []
+                for ve_ref in [r for r in relation if r]:
+                    if not relation:
+                        # @@@
+                        continue
+                    if version_obj == english_version:
+                        text_part_ref, position = ve_ref.rsplit(".t", maxsplit=1)
+                    else:
+                        text_part_ref, position = ve_ref.rsplit(".t", maxsplit=1)
+                        if position.startswith("t"):
+                            position = position[1:]
+
+                    text_part_urn = f"{version_obj.urn}{text_part_ref}"
+                    # TODO: compound Q objects query to minimize round trips
+                    tokens.append(
+                        Token.objects.get(text_part__urn=text_part_urn, position=position)
+                    )
+                relation_obj.tokens.set(tokens)
+
+english_version = Node.objects.get(urn="urn:cts:greekLit:tlg0012.tlg001.parrish-eng1:")
+create_alignment_records(new_pairs, greek_version, english_version)
